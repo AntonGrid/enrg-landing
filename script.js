64fe32eca0c0e5df4da36edf24c96275771a20c3
ENRG <enrg.project@gmail.com>
2026-05-04 16:11:20 +0300

    Добавлен энергетический фон, частицы и свечение

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+// Анимация фоновых частиц
+const canvas = document.getElementById('energyCanvas');
+const ctx = canvas.getContext('2d');
+let particles = [];
+const maxParticles = 100;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+class Particle {
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = Math.random() * 2 + 0.5;
+    this.speedX = (Math.random() - 0.5) * 0.5;
+    this.speedY = (Math.random() - 0.5) * 0.5;
+    this.opacity = Math.random() * 0.5 + 0.1;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+      this.reset();
+    }
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(0, 212, 170, ${this.opacity})`;
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particles = [];
+  for (let i = 0; i < maxParticles; i++) {
+    particles.push(new Particle());
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(p => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animateParticles);
+}
+
+resizeCanvas();
+initParticles();
+animateParticles();
+window.addEventListener('resize', () => {
+  resizeCanvas();
+  initParticles();
+});
+
+// Анимация при скролле
 const observerOptions = {
   threshold: 0.2,
   rootMargin: '0px 0px -50px 0px',
